8e027019276dae01a9f108bc2456bd56e91dbdcc
Akshay Sharma <akshaysharma@Akshays-MacBook-Air-2.local>
2022-01-28 04:17:33 -0500

    Adds Basic framework

--- backend/src/main.go
+++ backend/src/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// For this case, we will always pipe "Hello World" into the response writer
+	fmt.Fprintf(w, "Hello World!")
+}
+
+func newRouter() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", handler).Methods("GET")
+
+	staticFileDirectory := http.Dir("../../CEN5035-front-end/src/")
+
+	staticFileHandler := http.StripPrefix("/index", http.FileServer(staticFileDirectory))
+
+	r.PathPrefix("/index").Handler(staticFileHandler).Methods("GET")
+	return r
+}
 
 func main() {
-	fmt.Println("Hello")
+
+	r := newRouter()
+
+	r.HandleFunc("/hello", handler).Methods("GET")
+
+	http.ListenAndServe(":8080", r)
 }
