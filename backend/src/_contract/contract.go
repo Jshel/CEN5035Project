08b84fa3208ad3e3b9dfad9f99349e76c6a761ae
DrBubbles42 <22nickfox@gamil.com>
2022-03-01 14:36:56 -0500

    added endpoint for getting contracts and cleaned up some stuff

---
+++ backend/src/_contract/contract.go
@@ -0,0 +1,77 @@
+package contract
+
+import (
+	"fmt"
+	"encoding/json"
+
+	"net/http"
+	"github.com/jinzhu/gorm"
+	// load the sqlite driver
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+type Contract struct {
+	ContractID			string	`json:"contract_ID"`
+	ContractType		string	`json:"contract_type"`
+
+	DateCreated 		string 	`json:"date_created"`
+	TerminationDate		string 	`json:"termination_date"`
+
+	ValidSigniture 		bool	`json:"valid_signiture"`
+
+	PaymentType			string	`json:"payment_type"`
+	AmountPaid			float32	`json:"amount_paid"`
+	AmountOwed			float32 `json:"amount_owed"`
+
+	AttorneyName		string 	`json:"attorney_name`
+	AttorneyID			string 	`json:"attorney_ID"`
+
+	ClientName			string 	`json:"client_name"`
+	ClientID			string 	`json:"client_ID"`
+}
+
+var db *gorm.DB
+
+func InitContractDB(sqliteFile string, debugSQL bool){
+	_db, err := gorm.Open("sqlite3", sqliteFile)
+	if err != nil {
+		panic(fmt.Sprintf("Could not open management database %v", err))
+	}
+	if debugSQL {
+		db = _db.Debug()
+	} else {
+		db = _db
+	}
+
+	db.AutoMigrate(&Contract{})
+}
+
+func HandleGetContract() func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request){
+		// get url params as of now the search will just search for contract id from a given attorney
+		// the search for which attorney should be done with a cookie or session not by url query
+
+		// print values
+		values := r.URL.Query()
+		for k, v := range values {
+			fmt.Println(k, " => ", v)
+		}
+	
+		var attorneyID = r.URL.Query().get("attorneyID")
+		var contractID = r.URL.Query().get("contractID")
+
+		// Bring the contract in
+		contract := Contract{}
+		db.Where(&Contract{ContractID: contractID, AttorneyID: attorneyID}).Find(&contract)
+
+		//check if contract exists
+		if contract.ContractID != contractID{
+			http.Error(w, fmt.Sprintf("Contract ID: %s does not exist", contractID) http.StatusForbidden)
+			fmt.Println("ERROR: ", contractID, " does not exist for attorney ", attorneyID)
+			return
+		} else {
+			// contract exists
+			json.NewEncoder(w).Encode(contract)
+		}
+	}
+}
