217e93c573d16f4220387645ca0794eb1f7a63cb
DrBubbles42 <22nickfox@gamil.com>
2022-03-22 22:14:24 -0400

    Added an upload file function that is accessable b y posting a form to /api/upload. the function saves the pdf in the temp_contracts folder.

--- backend/src/main.go
+++ backend/src/main.go
@@ -4,6 +4,7 @@ import (
 	auth "attorneyManager/_auth"
 	contract "attorneyManager/_contract"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -19,21 +20,69 @@ func checkErr(err error) {
 	}
 }
 
-func main() {
-	fileServer := http.FileServer(http.Dir("../../CEN5035-front-end/src")) // use "../../CEN5035-front-end/src" for frontend, static is just for testing
-	http.Handle("/", fileServer)
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Uploading file...\n")
 
-	fmt.Println("Welcome to Attorney Manager! this is a basic setup in GO for the backend of the project.")
+	//parse input
+	r.ParseMultipartForm(10 << 20)
 
-	// database inits
-	auth.InitAuth("./user_database.db", false)
-	contract.InitContractDB("./contract_database.db", false)
+	//retreive files
+	file, handler, err := r.FormFile("contract")
+	if err != nil {
+		fmt.Println("Error getting file from form:")
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+	fmt.Println("Uploaded file name: ", handler.Filename)
+	fmt.Println("File size: ", handler.Size)
+	fmt.Println("MIME headder: ", handler.Header)
 
+	//write to a temp file
+	tempFile, err := ioutil.TempFile("temp_Contracts", "upload-*.pdf")
+	if err != nil {
+		fmt.Println("Error creating a temp file for the contract:")
+		fmt.Println(err)
+		return
+	}
+	defer tempFile.Close()
+
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println("Error creating a temp file for the contract:")
+		fmt.Println(err)
+		return
+	}
+
+	tempFile.Write(fileBytes)
+
+	//success
+	fmt.Fprintln(w, "Success uploading file!")
+}
+
+func setUpRoutes() {
 	// request handlers
 	http.HandleFunc("/api/login", auth.HandleLogin())
 	// http.HandleFunc("/api/logout", auth.HandleLogout())
 	http.HandleFunc("/api/create-account", auth.HandleRegister())
 	http.HandleFunc("/api/get-contract", contract.HandleGetContract())
+	http.HandleFunc("/api/upload", uploadFile)
+}
+
+func databaseInit() {
+	// database inits
+	auth.InitAuth("./user_database.db", false)
+	contract.InitContractDB("./contract_database.db", false)
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("../../CEN5035-front-end/src"))
+	http.Handle("/", fileServer)
+
+	fmt.Println("Welcome to Attorney Manager! this is a basic setup in GO for the backend of the project.")
+
+	databaseInit()
+	setUpRoutes()
 
 	fmt.Println("Starting server on port 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
