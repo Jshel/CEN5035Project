b87e3f9c18c61900d029f6c2e2ef02f89b48e359
Sunil Ghosal <sunilghosal@Sunils-MacBook-Pro.local>
2022-03-27 20:58:19 -0400

    created analytics card

---
+++ CEN5035-front-end/src/app/users/analytics-card/analytics-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-analytics-card',
+  templateUrl: './analytics-card.component.html',
+  styleUrls: ['./analytics-card.component.css']
+})
+export class AnalyticsCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- CEN5035-front-end/src/app/users/users.module.ts
+++ CEN5035-front-end/src/app/users/users.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { UsersRoutingModule } from './users-routing.module';
 import { UserListComponent } from './user-list/user-list.component';
 import { UserSingleComponent } from './user-single/user-single.component';
+import { AnalyticsCardComponent } from './analytics-card/analytics-card.component';
 
 
 @NgModule({
   declarations: [
     UserListComponent,
-    UserSingleComponent
+    UserSingleComponent,
+    AnalyticsCardComponent
   ],
   imports: [
     CommonModule,
