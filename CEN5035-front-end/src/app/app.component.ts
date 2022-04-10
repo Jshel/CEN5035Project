b5ec5816bb0a7d8f6da740468462ae7e86f520f4
Sunil Ghosal <sunilghosal@Sunils-MacBook-Pro.local>
2022-04-10 19:46:27 -0400

    feat: logging session

--- CEN5035-front-end/src/app/app.component.ts
+++ CEN5035-front-end/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http'
+import { User2 } from './login-form/login-form.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Contract Management';
+
+  readonly headers = new HttpHeaders().set('Content-Type', 'application/json')
+  constructor(
+    private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<User2>("/api/getuser").subscribe((response) => {console.log(response)}, err => {console.log("no session found")});
+  }
 }
